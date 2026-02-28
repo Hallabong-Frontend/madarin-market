bb8f07e10341a390c88a9fce4808319c24c19815
MingiKang <kmgi0312@gmail.com>
2026-02-28 10:56:36 +0900

    Profile.jsx 리팩토링

---
+++ src/components/profile/ProfileInfo.jsx
@@ -0,0 +1,68 @@
+import { useNavigate } from 'react-router-dom';
+import Avatar from '../common/Avatar';
+import ChatIcon from '../../assets/icons/icon-message-circle.svg?react';
+import ShareIcon from '../../assets/icons/icon-share.svg?react';
+import {
+  ProfileSection,
+  UserInfoRow,
+  StatItem,
+  StatNumber,
+  StatLabel,
+  UserDetails,
+  Username,
+  AccountId,
+  Intro,
+  ActionButtons,
+  IconActionBtn,
+  FollowButton,
+  EditButton,
+} from '../../pages/Profile/Profile.styles';
+
+const ProfileInfo = ({ profile, isMyProfile, following, handleFollow, handleChat }) => {
+  const navigate = useNavigate();
+
+  return (
+    <ProfileSection>
+      <UserInfoRow>
+        <StatItem onClick={() => navigate(`/profile/${profile.accountname}/follower`)}>
+          <StatNumber>{profile.followerCount}</StatNumber>
+          <StatLabel>followers</StatLabel>
+        </StatItem>
+
+        <Avatar src={profile.image} alt={profile.username} size="80px" border />
+
+        <StatItem onClick={() => navigate(`/profile/${profile.accountname}/following`)}>
+          <StatNumber>{profile.followingCount}</StatNumber>
+          <StatLabel>followings</StatLabel>
+        </StatItem>
+      </UserInfoRow>
+
+      <UserDetails>
+        <Username>{profile.username}</Username>
+        <AccountId>@ {profile.accountname}</AccountId>
+        {profile.intro && <Intro>{profile.intro}</Intro>}
+      </UserDetails>
+
+      {isMyProfile ? (
+        <ActionButtons>
+          <EditButton onClick={() => navigate('/profile/edit')}>프로필 수정</EditButton>
+          <EditButton onClick={() => navigate('/product/register')}>상품 등록</EditButton>
+        </ActionButtons>
+      ) : (
+        <ActionButtons>
+          <IconActionBtn onClick={handleChat}>
+            <ChatIcon />
+          </IconActionBtn>
+          <FollowButton $following={following} onClick={handleFollow}>
+            {following ? '언팔로우' : '팔로우'}
+          </FollowButton>
+          <IconActionBtn>
+            <ShareIcon />
+          </IconActionBtn>
+        </ActionButtons>
+      )}
+    </ProfileSection>
+  );
+};
+
+export default ProfileInfo;
---
+++ src/components/profile/ProfileProductList.jsx
@@ -0,0 +1,53 @@
+import { getImageUrl, formatPrice } from '../../utils/format';
+import { AI_DESC_SEPARATOR } from '../../constants/common';
+import SwiperLeftIconSvg from '../../assets/icons/icon-swiper-1.svg?react';
+import SwiperRightIconSvg from '../../assets/icons/icon-swiper-2.svg?react';
+import {
+  Section,
+  SectionTitle,
+  ProductSectionHeader,
+  ProductNavButtons,
+  ProductNavButton,
+  ProductList,
+  ProductCard,
+  ProductImage,
+  ProductName,
+  ProductPrice,
+} from '../../pages/Profile/Profile.styles';
+
+const ProfileProductList = ({ products, productListRef, handleProductClick, scrollProductList }) => {
+  if (!products || products.length === 0) return null;
+
+  return (
+    <Section>
+      <ProductSectionHeader>
+        <SectionTitle>판매 중인 상품</SectionTitle>
+        <ProductNavButtons>
+          <ProductNavButton type="button" aria-label="scroll products left" onClick={() => scrollProductList(-1)}>
+            <SwiperLeftIconSvg />
+          </ProductNavButton>
+          <ProductNavButton type="button" aria-label="scroll products right" onClick={() => scrollProductList(1)}>
+            <SwiperRightIconSvg />
+          </ProductNavButton>
+        </ProductNavButtons>
+      </ProductSectionHeader>
+      <ProductList ref={productListRef}>
+        {products.map((product) => (
+          <ProductCard key={product.id} onClick={() => handleProductClick(product)}>
+            <ProductImage
+              src={getImageUrl(product.itemImage)}
+              alt={product.itemName}
+              onError={(e) => {
+                e.target.src = 'https://via.placeholder.com/130?text=No+Image';
+              }}
+            />
+            <ProductName>{product.itemName.split(AI_DESC_SEPARATOR)[0].trim()}</ProductName>
+            <ProductPrice>{formatPrice(product.price)}원</ProductPrice>
+          </ProductCard>
+        ))}
+      </ProductList>
+    </Section>
+  );
+};
+
+export default ProfileProductList;
